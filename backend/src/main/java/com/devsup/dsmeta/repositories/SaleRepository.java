4e01e1164da53faca399f5cbfdf67e7294e19242
Eiji <47559146+guieiji@users.noreply.github.com>
2022-07-13 15:23:46 -0300

    API test

---
+++ backend/src/main/java/com/devsup/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsup.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsup.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
